6f07b6aeec8f39942e62d1886c3a220b35857c07
Andrew Onyancha <mr.onyanchaandrew@gmail.com>
2022-08-25 23:51:32 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,6 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heart = {
+  '♡':FULL_HEART,
+  '♥':EMPTY_HEART
+}
+// Your JavaScript code goes here!
+const like = document.querySelectorAll(' footer > ul > li > span')
+
+console.log(like)
+for (let i =0;i<like.length;i++){
+  like[i].addEventListener('click',(e)=>{
+    mimicServerCall()
+    .then(success=>{
+      alert(success)
+      like[i].innerText = heart[like[i].innerText]
+
+       })
+    .catch(error => {
+      document.querySelector('#modal').classList.remove()
+
+      alert(error)
+      setTimeout(()=>document.querySelector('#modal').classList.add('hidden'),3*1e3)
+      })  //if you double click w/o check of server-alert
+   })
+
+}
+
 
 
 
